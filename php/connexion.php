0b8af3e5cd128d83502271135fef922af287a719
lokioh <loiclim.etu@gmail.com>
2020-10-18 17:45:38 +0200

    modif du 18/10/2020

--- php/connexion.php
+++ php/connexion.php
@@ -1,5 +1,17 @@
 <?php
 
+$hostname = "localhost"; //ou vs-wamp
+$base = "bd";
+$loginBD = "root";
+$passBD = "";
 
+try{
+
+    $bd = new PDO ("myslq:server=$hostname; dbname=$base", "$loginBD", "$passBD");
+}
+
+catch (PDOException $e) {
+    die ("Echech de connexion : " . $e->getMessage() . "\n");
+}
 
 ?>
