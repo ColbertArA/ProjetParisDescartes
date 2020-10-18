fc217807036c66ee6fe128dd623031bf729fc352
lokioh <loiclim.etu@gmail.com>
2020-10-18 21:44:57 +0200

    connexion bd ok

--- modele/connect.php
+++ modele/connect.php
@@ -13,10 +13,4 @@ catch (PDOException $e) {
     die ("Echec de connexion :" . $e->getMessage() . "\n");
 }
 
-///////////////////////////////////////////
-//Voici 2 lignes pour tester la connexion seule, en invoquant ce fichier.
-//   Eliminer ces 2 lignes si le test est réussi !
-//		$ok = 'connexion ok';
-//		die ($ok); 
-
 ?>
