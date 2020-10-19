231b9b6061e8b9a58b8c1647108a7c768e1c5670
lokioh <loiclim.etu@gmail.com>
2020-10-19 13:50:54 +0200

    19/10

--- modele/inscription.php
+++ modele/inscription.php
@@ -1,28 +1,17 @@
 <?php
 
-$nom_client = isset($_POST['nom'])?($_POST['nom']):'';
-$mdp_client = isset($_POST['mdp'])?($_POST['mdp']):'';
-$mail_client = isset($_POST['mail'])?($_POST['mail']):'';
+$nom_client = $_POST['nom'];
+$mdp_client = $_POST['mdp'];
+$mail_client = $_POST['mail'];
 
-if (count($_POST)==0){
-    require ("./vue/index.html") ;
-    else{
-        inscription($nom_client, $mdp_client, $mail_client);
-    }
-}
-
+inscription($nom_client, $mdp_client, $mail_client);
 
 function inscription($nom, $mdp, $mail){
     //connexion à la base de données
     require("./modele/connect.php");
 
     //insertion des données
-    $req = $bdd->prepare('INSERT INTO client(nom_client, mdp_client, mail_client) VALUES(:nom, :mdp, :mail)');
-    $req->execute(array(
-        'nom' => $nom,
-        'mdp' => sha($mdp),
-        'mail' => $mail
-    ));
+    $req = $bdd->prepare('INSERT INTO client(nom_client, mdp_client, mail_client) VALUES($nom, $mdp, $mail)');
 
     echo 'Inscription réussie !'
 }
