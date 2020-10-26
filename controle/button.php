ad9d04dbdb856e12a12acabc826f08e01297c7ee
lokioh <loiclim.etu@gmail.com>
2020-10-26 11:24:11 +0100

    26/10

--- controle/button.php
+++ controle/button.php
@@ -1,24 +1,10 @@
 <?php
 
-//fichier php permettant acceder les templates à partir des bouttons
-
-//lien pour les bouttons inscriptions
-function inscription() {
-    require ("./vue/tpl/inscription.tpl");
-}
-
-//lien pour les bouttons connexions
-function connexion() {
-    require ("./vue/tpl/connexion.tpl");
-}
+//fichier php permettant d'afficher l'accueil du site
 
 //lien pour les bouttons acceuils
 function gogo() {
     require ("./vue/ident.tpl");
 }
 
-//lien vers la page connexion_err en cas de mauvais mot de passe ou mail
-function err_connexion() {
-    require ('./vue/tpl_err/connexion_err.tpl');
-}
 ?>
